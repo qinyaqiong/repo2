10eff5dd03901cf78e3254d6ca69324dd0553b31
qinyaqiong <itcast@163>
2019-04-12 21:15:38 +0800

    kelong

--- git_frist/src/com/itheima/git/Demo.java
+++ git_frist/src/com/itheima/git/Demo.java
@@ -4,5 +4,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("git......");
         System.out.println("git1....");
+
+        System.out.println("clone...");
     }
 }
