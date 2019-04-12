5cef076283eed487bd438523f4fb3f105225f76d
qinyaqiong <itcast@163>
2019-04-12 20:56:05 +0800

    main2

--- git_frist/src/com/itheima/git/Demo.java
+++ git_frist/src/com/itheima/git/Demo.java
@@ -3,5 +3,6 @@ package com.itheima.git;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("git......");
+        System.out.println("git1....");
     }
 }
